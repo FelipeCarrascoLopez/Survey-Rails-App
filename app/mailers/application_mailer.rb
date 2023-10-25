fb8389cea0c632a6e41e980ce9e2d51add45e481
macbook <macbook@MacBook-Pro-de-macbook.local>
2023-10-25 16:47:32 -0300

    proyecto surveys app

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "yorch.academy@gmail.com"
+  layout "mailer"
+end
---
+++ config/tailwind.config.js
@@ -0,0 +1,39 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    colors: {
+      "mainColor": "#2c3639",
+      "white": "#ffffff",
+      "black": "#000000",
+      "signIn": "#9ff1e5",
+      "signUp": "#ecffc8",
+      "fontColor": "#0c5d75",
+      "footerGray": "#f5f5f5",
+      "formBack": "#1f2937",
+      "editIcon": "#047487",
+      "removeIcon": "#920404",
+      "evaluationStrong": "#126f05",
+      "evaluationLight": "#ddffd9",
+      "chapterStrong": "#05636f",
+      "surveyStrong": "#686f05",
+      "errorMessage": "#fecaca"
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ]
+}
